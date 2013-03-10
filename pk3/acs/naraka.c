2db2a2671546f471476519dc323bdaccd0a7dbac
Ijon Tichy <gztichy@lavabit.com>
2013-03-09 21:01:33 -0600

    The cyberdemon dash is no longer 7 times as long as it should be.

--- pk3/acs/naraka.c
+++ pk3/acs/naraka.c
@@ -115,7 +115,7 @@ script 474 (int cybermove) // It's time to D-D-D-DASH!
             GiveInventory("CyberdemonDashCount", 42);
             delay(30);
             GiveInventory("CyberDash", 1);
-            ACS_ExecuteAlways(NARAKA_COOLDOWN, 0, 2, 7, 474);
+            ACS_ExecuteAlways(NARAKA_COOLDOWN, 0, 2, 1, 474);
         }
         break;
 

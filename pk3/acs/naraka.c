2265dcfe25367cd9300dfebd37207aeddf6eb77f
TerminusEst13 <terminusest13@gmail.com>
2013-08-25 20:45:24 -0400

    When more than four players are on the Villain team, they may no longer have even team numbers with the Heroes. This should help give people the idea that, hey, stack on the Heroes.

--- pk3/acs/naraka.c
+++ pk3/acs/naraka.c
@@ -641,9 +641,9 @@ script 895 ENTER clientside
 	{
 		if (PlayerTeam() == 1)
 		{
-		    if (GetTeamProperty(1,TPROP_NumPlayers) >= 4)
+		    if (GetTeamProperty(1,TPROP_NumPlayers) > 4)
 		    {
-			    if (GetTeamProperty(1,TPROP_NumPlayers) > GetTeamProperty(0,TPROP_NumPlayers))
+			    if (GetTeamProperty(1,TPROP_NumPlayers) >= GetTeamProperty(0,TPROP_NumPlayers))
 			    { Print(s:"\cgVillains\cf have too many players!");
 				ConsoleCommand("spectate"); }
 		    }

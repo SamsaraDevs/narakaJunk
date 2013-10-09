7a1f34d78dfca0e0b5c763bbf33f7eb0ba91d577
TerminusEst13 <terminusest13@gmail.com>
2013-10-09 18:16:25 -0400

    ingamechangelog.txt

--- pk3/acs/narakaDefs.h
+++ pk3/acs/narakaDefs.h
@@ -67,31 +67,26 @@ int LMSUniques[CLASSCOUNT] =
 };
 
 int ChangelogNaraka =
-"FROM V0.11 TO V0.12:\n\
+"FROM v0.12 TO v0.13:\n\
 ========================\n\
-- Korax's speed has been slightly increased, as has the speed and damage of his Ethereal Wraith magic.\n\
-- Coop powerups for the villains have had their power slightly enhanced.\n\
-- Villains now get CoopModeOn when playing in coop games.\n\
-- Korax's Green/Brown Chaos Magics have been combined into one Chaos Serpent Magic.\n\
-- Korax now has Steel Chain magic to grab enemies at long range and yank them over to him.\n\
-- Team balancer now runs after 5 people join the game, and now only runs on the joining team.\n\
-- Vllain pickup messages no longer display globally.\n\
-- D'Sparil can now dismount after respawning.\n\
-- Tfear's Flawed now runs off Light ammo, rather than Heavy.\n\
-- Admiral Tfear now has a new weapon: The Predator Type 'Abandon', a rapid-fire grenade launcher that arcs low and bounces, allowing him to cover an area.\n\
-- The Hatred's projectiles now move a slight bit faster, for slightly easier hitting.\n\
-- Guilt's lunge no longer uses ammo, and comes out slightly faster.\n\
-- Korax's death sequence has been completely reworked in DECORATE, thanks to Popsoap.\n\
-- Admiral Tfear did not have any CoopMode or TLMS mode checks for his weapons, meaning all his attacks would get stuck on allies. This has been fixed.\n\
-- The Cyberdemon and D'Sparil now have proper bar HUDs. Only two more to go!\n\
-- Spectating unmounted D'Sparil now no longer results in an HOM.\n\
-- Tfear's Pfhor summons now can no longer be summoned in the environment and rendered useless.\n\
-- Tfear's Pfhor now have a more Marathon-esque teleport sequence.\n\
-- The HUDs now work in the automap.\n\
-- Admiral Tfear now has (some of) his proper sprites!\n\
-- Summoned Pfhor now inherit colors from their summoner.\n\
-- By popular request, there is now a Mana Jewel at the bottom of Korax's HUD, indicating what weapon is equipped by color association.\n\
-- Heroes and Villains can now work side by side in co-op without getting stuck on each other.\n\
-- Minions no longer bring Invasion to a screeching halt.\n\
-- Up to seven Pfhor Troopers can now be summoned. Their max damage range has been decreased from 15 to 10, and their accuracy decreased.\n\
-- Hitler, the Villain of B.J. Blazkowicz, has been added!";
+- Pfhor no longer repeat the victim's name in place of the killer's name.\n\
+- The Team Balancer no longer checks to see if there are X amount of player in the game. Instead, it checks if X amount of players are on either team. This should result in much more lenient balancing when there's a small number of players.\n\
+- Joining the game at the last minute no longer breaks the balancer.\n\
+- Hitler now always gains his speed boost when he loses his mech.\n\
+- Tfear's Flawed no longer uses the SYSTEM ERROR obituary.\n\
+- Minions now have +FRIENDLY in the co-op version, which means they should now attack enemy monsters.\n\
+- Hitler's 10% damage resistance buff now only takes effect in co-op play as his 'Beef' item.\n\
+- D'Sparil's cooldown for his summon and Chaos Device has been doubled--but whenever he enters his Pain State when on-foot, the cooldown is shortened slightly. This should reward more aggressive D'Sparils and slow down those who simply teleport, summon, and repeat.\n\
+- Likewise, Tfear's cooldown for his Beacon has been doubled, and whenever he enters his Pain state the cooldown is shortened.\n\
+- Tfear's questionable shotgun-esque alt-fire for the Abandon has been replaced with a more-suitable-for-the-role remote grenade launcher.\n\
+- The powerups for the Shotgun and the Chaingun have been replaced with something more useful--very slight armor and health regeneration.\n\
+- The pickup messages have been reworked so that people actually know what you're getting on picking them up.\n\
+- D'Sparil, Korax, and Cyberdemon are now less resistant to Duke's explosives. Hitler is now slightly more resistant.\n\
+- By massive request, Disciples no longer drop Lesser Tomes of Power. Bye bye, awesome book. ;_;\n\
+- All of the Villains (sans D'Sparil due to technical issues) now have different colors across their life/armor bar to represent how much they have.\n\
+- Due to popular demand, Hitler's obituary has been changed.\n\
+- naraka_ban[class], naraka_[class]defense, and naraka_[class]damage have been imported. If you think a class is too OP, UP, or broken, now you have control over them!\n\
+- Tfear now has a HUD graphic composed of Marathon Pfhor textures.\n\
+- When sv_weaponstay is enabled, villains get a flash on their screen when they pick up a powerup.\n\
+- The Villains now have their respective individual colors, like the Heroes do.\n\
+- The team balancer has been turned into a cvar, naraka_teambalancer. 0 does not attempt to balance the teams at all, 1 favors Heroes but allows Villains to have equal numbers with the heroes if desired, 2 is more strict and does not allow Villains to have equal numbers. Defaults to 1.";

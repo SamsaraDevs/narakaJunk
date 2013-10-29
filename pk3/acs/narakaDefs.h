22c6d85d4855438b144ac17228624cd0e55ca55c
TerminusEst13 <terminusest13@gmail.com>
2013-10-29 17:14:24 -0400

    Ingame changelog updated.

--- pk3/acs/narakaDefs.h
+++ pk3/acs/narakaDefs.h
@@ -89,4 +89,23 @@ int ChangelogNaraka =
 - Tfear now has a HUD graphic composed of Marathon Pfhor textures.\n\
 - When sv_weaponstay is enabled, villains get a flash on their screen when they pick up a powerup.\n\
 - The Villains now have their respective individual colors, like the Heroes do.\n\
-- The team balancer has been turned into a cvar, naraka_teambalancer. 0 does not attempt to balance the teams at all, 1 favors Heroes but allows Villains to have equal numbers with the heroes if desired, 2 is more strict and does not allow Villains to have equal numbers. Defaults to 1.";
+- The team balancer has been turned into a cvar, naraka_teambalancer. 0 does not attempt to balance the teams at all, 1 favors Heroes but allows Villains to have equal numbers with the heroes if desired, 2 is more strict and does not allow Villains to have equal numbers. Defaults to 1.\n\
+- Team balancer should no longer take effect when people are respawning.\n\
+- After picking up a certain number of backpacks, Heroes are no longer unable to pick up any more ammo.\n\
+- Tfear's beacon limiter now no longer exists for his Pfhor Troopers, thanks to lack of APROP_MASTERTID. To make up for it, they are slightly less triggerhappy, always trigger their painchance, and have less HP.\n\
+- Tfear's beacon now no longer summons TLMS minions in coop.\n\
+- D'Sparil's minions can no longer be revived by Archviles.\n\
+- When respawning, unarmored Hitler no longer loses his speed boost.\n\
+- In coop, Hitler's armored/unarmored damage per volley has been increased from 24/12 to 36/18.\n\
+- Hitler's weapons now have the Villain damagetype, meaning they should no longer injure allied minions.\n\
+- When D'Sparil's Serpent is killed in coop, D'Sparil was not shifting species to 'Player'. This has been fixed.\n\
+- Villain HUDs now support Super Turbo Turkey Puncher 3000.\n\
+- In coop, minions should now be immune to: DukeExplosive, Explosive, BFGSpray, Zorch, BJSpear, HexenIce, Poison, PoisonCloud, FireExplosive, Wraithverge, MightyBoot, DukeFreezethrower, WMCBeam, WMCNade, Chainsaw, TimonsAxe, Pistol, CorvusHellstaff, ZorchPistol, and Telefrag. While its not EVERYTHING they can be hit with, it should dramatically reduce their able-to-be-TK'd potential.\n\
+- Minions no longer can activate teleports or Monster Crosses lines.\n\
+- Minions no longer can go beyond monster boundaries.\n\
+- Fiery attacks from Villains now have the VillainTeamFire damagetype, which should cause the appropriate burn death in Heroes in TLMS play.\n\
+- Cyberdemon can no longer hold down fire to Cyber-run and shoot at the same time.\n\
+- Cyberdemon targeter no longer appears on automap hud. Now does appear on fullscreen hud.\n\
+- D'Sparil now can only use the Chaos Device thrice in co-op before it goes inert for the level.\n\
+- Hitler now has a VII. Only three more to go!\n\
+- Sounds have been rearranged and put into their own folders.";
